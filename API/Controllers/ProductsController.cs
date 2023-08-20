f4f9ef9a54a532eff469cf0cbb7a3c82a8fc58ac
alexanderthegreat99 <darren.alex.perez>
2023-08-20 17:13:36 -0400

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 // using API.Entities;
 
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,38 +13,42 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+       private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
+
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-             var products = await _context.Products.ToListAsync();
+             var products = await _repo.GetProductsAsync();
 
-            return products;
+            return Ok(products);
             
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            var products = await _repo.GetProductByIdAsync(id);
+
+            return Ok(products);
            
         }
         
-        // [HttpGet]
-        // public string GetProducts()
-        // {
-        //     return "This will be a list";
-        // }
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
 
-        // [HttpGet("{id}")]
-        // public string GetProduct(int id)
-        // {
-        //     return "This will be a single product";
-        // }
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 
 using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +12,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
@@ -20,5 +21,19 @@ var app = builder.Build();
 //app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+   logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
