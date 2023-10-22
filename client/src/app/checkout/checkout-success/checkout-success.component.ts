1297d34414374a539e337aff30f82444be6bdaf4
alexanderthegreat99 <darren.alex.perez>
2023-10-22 18:48:50 -0400

    End of section 20

--- client/src/app/checkout/checkout-success/checkout-success.component.ts
+++ client/src/app/checkout/checkout-success/checkout-success.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Order } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-checkout-success',
@@ -7,7 +8,10 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./checkout-success.component.scss']
 })
 export class CheckoutSuccessComponent {
+  order?: Order;
 
-
-
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.order = navigation?.extras?.state as Order
+  }
 }
